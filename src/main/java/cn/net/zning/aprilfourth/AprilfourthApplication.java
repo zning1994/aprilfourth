9a73fc5cd1b6832c90bfd31d64ddb269f80d12e9
ZNing <zhn038@gmail.com>
2020-09-09 17:28:00 +0800

    初始化仓库 2020-9-9 17:27:51

---
+++ src/main/java/cn/net/zning/aprilfourth/AprilfourthApplication.java
@@ -0,0 +1,13 @@
+package cn.net.zning.aprilfourth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilfourthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprilfourthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/net/zning/aprilfourth/AprilfourthApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.zning.aprilfourth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilfourthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
